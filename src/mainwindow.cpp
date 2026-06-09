67589bf4072335f4fce062674a4af18cbb1a3e20
Maximilian Berkes <heggo@heggo.dev>
2026-06-10 01:00:10 +0200

    feat: allow choosing categories and displaying its units
    It uses absolute position for now so it doesn't resize well but it works

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,19 +1,38 @@
 #include "mainwindow.hpp"
 
-#include <QDebug>
+#include <format>
 
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-    qDebug() << "10 meters to miles:\n" << service.convert(10, "meter", "mile") << "\n";
-    const auto categories = service.categories();
-    qDebug() << service.categories() << "\n";
+    ui->setupUi(this);
 
-    for (const auto& cat : categories) {
-        qDebug() << service.units(cat) << "\n";
+    const auto units = service.units("length");
+
+    for (const auto& [id, symbol] : units) {
+        ui->unitList->addItem(QString::fromStdString(std::format("{} ({})", id, symbol)));
     }
 
-    ui->setupUi(this);
+    ui->categoryList->hide();
+    connect(ui->sidebarButton, &QPushButton::toggled, ui->categoryList, &QWidget::setVisible);
+
+    const auto categories = service.categories();
+
+    for (const auto& category : categories) {
+        ui->categoryList->addItem(QString::fromStdString(category));
+    }
+    connect(ui->categoryList, &QListWidget::currentTextChanged, this, [this](const QString& categoryName) {
+        ui->labelCategoryTitle->setText(categoryName);
+        ui->unitList->clear();
+
+        const auto units = service.units(categoryName.toStdString());
+
+        for (const auto& [id, symbol] : units) {
+            ui->unitList->addItem(QString::fromStdString(std::format("{} ({})", id, symbol)));
+        }
+        ui->categoryList->hide();
+        ui->sidebarButton->setChecked(false);
+    });
 }
 
 MainWindow::~MainWindow() { delete ui; }
