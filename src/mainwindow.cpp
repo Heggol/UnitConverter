ce640959d198c94b795432ae9a5906f83edb73f1
Maximilian Berkes <heggo@heggo.dev>
2026-06-11 23:01:52 +0200

    feat: better ui, working conversion and some additional energy units
    The inputs for units are definitely not the right ones so I'll change them out soon

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.hpp"
 
 #include <QGraphicsDropShadowEffect>
-#include <format>
 
 #include "ui_mainwindow.h"
 
@@ -10,9 +9,13 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 
     const auto units = service.units("length");
 
-    for (const auto& [id, symbol] : units) {
-        ui->unitList->addItem(QString::fromStdString(std::format("{} ({})", id, symbol)));
+    for (const auto& id : units | std::views::keys) {
+        QString currentUnit = QString::fromStdString(id);
+        currentUnit[0] = currentUnit[0].toUpper();
+        ui->unitDropdownR->addItem(currentUnit);
+        ui->unitDropdownL->addItem(currentUnit);
     }
+    ui->unitDropdownR->setCurrentIndex(1);
 
     ui->categoryList->hide();
     connect(ui->sidebarButton, &QPushButton::toggled, ui->categoryList, &QWidget::setVisible);
@@ -25,19 +28,49 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
     const auto categories = service.categories();
 
     for (const auto& category : categories) {
-        ui->categoryList->addItem(QString::fromStdString(category));
+        QString currentCategory = QString::fromStdString(category);
+        currentCategory[0] = currentCategory[0].toUpper();
+        ui->categoryList->addItem(currentCategory);
     }
+
     connect(ui->categoryList, &QListWidget::currentTextChanged, this, [this](const QString& categoryName) {
+        if (categoryName.isEmpty()) return;
+
         ui->labelCategoryTitle->setText(categoryName);
-        ui->unitList->clear();
+        ui->unitDropdownR->clear();
+        ui->unitDropdownL->clear();
 
-        const auto units = service.units(categoryName.toStdString());
+        const auto units = service.units(categoryName.toLower().toStdString());
 
-        for (const auto& [id, symbol] : units) {
-            ui->unitList->addItem(QString::fromStdString(std::format("{} ({})", id, symbol)));
+        for (const auto& id : units | std::views::keys) {
+            ui->unitDropdownR->addItem(QString::fromStdString(id));
+            ui->unitDropdownL->addItem(QString::fromStdString(id));
         }
+        ui->unitDropdownR->setCurrentIndex(1);
         ui->categoryList->hide();
         ui->sidebarButton->setChecked(false);
+    });
+
+    ui->inputR->setMaximum(std::numeric_limits<double>::max());
+    ui->inputR->setDecimals(10);
+    ui->inputL->setMaximum(std::numeric_limits<double>::max());
+    ui->inputL->setDecimals(10);
+
+    connect(ui->inputR, &QDoubleSpinBox::valueChanged, this, [this](const double inputValue) {
+        const auto fromUnit = ui->unitDropdownR->currentText().toLower().toStdString();
+        const auto toUnit = ui->unitDropdownL->currentText().toLower().toStdString();
+        const auto result = service.convert(inputValue, fromUnit, toUnit);
+        ui->inputL->blockSignals(true);
+        ui->inputL->setValue(result);
+        ui->inputL->blockSignals(false);
+    });
+    connect(ui->inputL, &QDoubleSpinBox::valueChanged, this, [this](const double inputValue) {
+        const auto fromUnit = ui->unitDropdownL->currentText().toLower().toStdString();
+        const auto toUnit = ui->unitDropdownR->currentText().toLower().toStdString();
+        const auto result = service.convert(inputValue, fromUnit, toUnit);
+        ui->inputR->blockSignals(true);
+        ui->inputR->setValue(result);
+        ui->inputR->blockSignals(false);
     });
 }
 
